d350b7b2d75efed6d8d3e50f5a63c5880c1247a1
Koras02 <wjdgh0727@gmail.com>
2023-02-01 19:20:56 +0900

    example nodejs-graphql

---
+++ server.js
@@ -0,0 +1,28 @@
+let express = require("express");
+let { graphql, graphqlHTTP } = require("express-graphql");
+let { buildSchema } = require("graphql");
+
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+let root = {
+  hello: () => {
+    return "Hello World";
+  },
+};
+
+let app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
